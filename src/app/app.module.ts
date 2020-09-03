6838269f8e4eb3f8f49d4e3392009afd38d019a3
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2020-09-02 21:04:02 -0700

    implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GroceryComponent } from './grocery/grocery.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GroceryComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, GroceryComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GroceryComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -3,13 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grocery',
   templateUrl: './grocery.component.html',
-  styleUrls: ['./grocery.component.css']
+  styleUrls: ['../../assets/css/bootstrap.min.css'],
 })
 export class GroceryComponent implements OnInit {
+  
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  task = {
+    name: '',
+    id: 0,
+  };
+  tasks = [];
 
-  ngOnInit(): void {
+  onClick() {
+    if (this.task.id == 0) {
+      this.tasks.push({
+        id: new Date().getTime(),
+        name: this.task.name,
+        strike: false,
+      });
+    }
+
+    this.task = {
+      name: '',
+      id: 0,
+    };
   }
 
+  onEdit(item) {
+    this.task = item;
+  }
+
+  onDelete(item) {
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (item.id == this.tasks[i].id) {
+        this.tasks.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  onStrike(item) {
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (item.id == this.tasks[i].id) {
+        if (this.tasks[i].strike) {
+          this.tasks[i].strike = false;
+        } else {
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
 }
